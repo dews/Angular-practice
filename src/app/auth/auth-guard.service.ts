bbc5e43ceeb62e2a5dc71050e90f50e443cc490d
calvinzheng <calvinzheng@exosite.com>
2018-02-24 19:06:37 +0800

    fixup! Reduce file size by import needed

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,21 +1,28 @@
 import { CanActivate, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, Route, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
     constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
+    canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
         return new Promise(resolve => {
-            setTimeout(() => {
-                if (this.authService.isAuthenticated()) {
-                    resolve(true);
-                } else {
-                    this.router.navigate(['/signin']);
-                    resolve(false);
-                }
-            }, 500);
+            if (this.authService.isAuthenticated()) {
+                resolve(true);
+            } else {
+                const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+                    unsubscribe();
+                    if (user) {
+                        resolve(true);
+                    } else {
+                        this.router.navigate(['/signin']);
+                        resolve(false);
+                    }
+                });
+            }
         });
     }
     canLoad(route: Route): boolean {
